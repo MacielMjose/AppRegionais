e9efed5edceeca64f544ed8ae405c2dd1fb122d9
MacielMjose <jlmm92.maciel@gmail.com>
2019-05-23 00:55:10 -0300

    criação da Classe Swipe Listener

---
+++ app/src/main/java/com/cursoandroi/regionaisapp/OnSwipeTouchListener.java
@@ -0,0 +1,53 @@
+package com.cursoandroi.regionaisapp;
+
+
+import android.content.Context;
+import android.view.GestureDetector;
+import android.view.GestureDetector.SimpleOnGestureListener;
+import android.view.MotionEvent;
+import android.view.View;
+
+public class OnSwipeTouchListener implements View.OnTouchListener {
+
+    private final GestureDetector gestureDetector;
+
+    public OnSwipeTouchListener (Context context){
+
+        gestureDetector = new GestureDetector(context, new GestureDetector.OnGestureListener() {
+
+            @Override
+            public boolean onDown(MotionEvent e) {
+                return false;
+            }
+
+            @Override
+            public void onShowPress(MotionEvent e) {
+
+            }
+
+            @Override
+            public boolean onSingleTapUp(MotionEvent e) {
+                return false;
+            }
+
+            @Override
+            public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+                return false;
+            }
+
+            @Override
+            public void onLongPress(MotionEvent e) {
+
+            }
+
+            @Override
+            public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+                return false;
+            }
+        }
+
+
+    }
+
+
+}
